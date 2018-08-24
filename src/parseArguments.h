c87cf72d6b9e7daa6cd129295ef8a302e33b17bc
kdominik <k-dominik@users.noreply.github.com>
2018-08-24 09:36:22 +0200

    added PATH_MAX definition as used in cpython

--- src/parseArguments.h
+++ src/parseArguments.h
@@ -1,3 +1,35 @@
+
+/* Operating system dependencies */
+// blatantly copied from cpython source code
+#ifdef MS_WINDOWS
+#define SEP L'\\'
+#define ALTSEP L'/'
+#define MAXPATHLEN 256
+#define DELIM L';'
+#endif
+
+/* Filename separator */
+#ifndef SEP
+#define SEP L'/'
+#endif
+
+/* Max pathname length */
+#ifdef __hpux
+#include <sys/param.h>
+#include <limits.h>
+#ifndef PATH_MAX
+#define PATH_MAX MAXPATHLEN
+#endif
+#endif
+
+#ifndef MAXPATHLEN
+#if defined(PATH_MAX) && PATH_MAX > 1024
+#define MAXPATHLEN PATH_MAX
+#else
+#define MAXPATHLEN 1024
+#endif
+#endif
+
 char *realpathEx(const char *path, char *buff) {
     char *home;
     if (*path=='~' && (home = getenv("HOME"))) {
