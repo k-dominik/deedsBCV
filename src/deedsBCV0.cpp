1865912537e78b115cd7c94745583ff4a6c8af6c
kdominik <k-dominik@users.noreply.github.com>
2018-08-26 18:27:37 +0200

    switched to chrono

--- src/deedsBCV0.cpp
+++ src/deedsBCV0.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <math.h>
-#include <sys/time.h>
+#include <chrono>
 #include <vector>
 #include <algorithm>
 #include <numeric>
@@ -111,7 +111,7 @@ int main (int argc, char * const argv[]) {
                       
     //READ IMAGES and INITIALISE ARRAYS
     
-    timeval time1,time2,time1a,time2a;
+    chrono::time_point<chrono::high_resolution_clock> time1,time2,time1a,time2a;
 
 	RAND_SAMPLES=1; //fixed/efficient random sampling strategy
 	
@@ -214,8 +214,8 @@ int main (int argc, char * const argv[]) {
     uint64_t* im1b_mind=new uint64_t[m*n*o];
     uint64_t* warped_mind=new uint64_t[m*n*o];
 	
-	gettimeofday(&time1a, NULL);
-    float timeDataSmooth=0;
+	time1a = chrono::high_resolution_clock::now();
+    chrono::duration<double> timeDataSmooth;
 	//==========================================================================================
 	//==========================================================================================
 
@@ -225,14 +225,14 @@ int main (int argc, char * const argv[]) {
         float prev=mind_step[max(level-1,0)];//max(min(label_quant[max(level-1,0)],2.0f),1.0f);
         float curr=mind_step[level];//max(min(label_quant[level],2.0f),1.0f);
         
-        float timeMIND=0; float timeSmooth=0; float timeData=0; float timeTrans=0;
+        chrono::duration<float> timeMIND, timeSmooth, timeData, timeTrans;
 		
         if(level==0|prev!=curr){
-            gettimeofday(&time1, NULL);
+            time1 = chrono::high_resolution_clock::now();
             descriptor(im1_mind,warped0,m,n,o,mind_step[level]);//im1 affine
             descriptor(im1b_mind,im1b,m,n,o,mind_step[level]);
-            gettimeofday(&time2, NULL);
-            timeMIND+=time2.tv_sec+time2.tv_usec/1e6-(time1.tv_sec+time1.tv_usec/1e6);
+            time2 = chrono::high_resolution_clock::now();
+            timeMIND = time2 - time1;
 		}
 		
 		step1=args.grid_spacing[level];
@@ -250,67 +250,68 @@ int main (int argc, char * const argv[]) {
 		cout<<"==========================================================\n";
 		
         //FULL-REGISTRATION FORWARDS
-        gettimeofday(&time1, NULL);
+        time1 = chrono::high_resolution_clock::now();
 		upsampleDeformationsCL(u0,v0,w0,u1,v1,w1,m1,n1,o1,m2,n2,o2);
         upsampleDeformationsCL(ux,vx,wx,u0,v0,w0,m,n,o,m1,n1,o1);
         //float dist=landmarkDistance(ux,vx,wx,m,n,o,distsmm,casenum);
 		warpAffine(warped1,im1,im1b,X,ux,vx,wx);
 		u1=new float[sz1]; v1=new float[sz1]; w1=new float[sz1];
-        gettimeofday(&time2, NULL);
-		timeTrans+=time2.tv_sec+time2.tv_usec/1e6-(time1.tv_sec+time1.tv_usec/1e6);
+        time2 = chrono::high_resolution_clock::now();
+		timeTrans = time2 - time1;
         cout<<"T"<<flush;
-        gettimeofday(&time1, NULL);
+        time1 = chrono::high_resolution_clock::now();
 		descriptor(warped_mind,warped1,m,n,o,mind_step[level]);
 
-        gettimeofday(&time2, NULL);
-		timeMIND+=time2.tv_sec+time2.tv_usec/1e6-(time1.tv_sec+time1.tv_usec/1e6);
+        time2 = chrono::high_resolution_clock::now();
+		timeMIND += time2 - time1;
         cout<<"M"<<flush;
-        gettimeofday(&time1, NULL);
+        time1 = chrono::high_resolution_clock::now();
         dataCostCL((unsigned long*)im1b_mind,(unsigned long*)warped_mind,costall,m,n,o,len3,step1,hw1,quant1,args.alpha,RAND_SAMPLES);
-        gettimeofday(&time2, NULL);
+        time2 = chrono::high_resolution_clock::now();
 
-		timeData+=time2.tv_sec+time2.tv_usec/1e6-(time1.tv_sec+time1.tv_usec/1e6);
+		timeData = time2 - time1;
         cout<<"D"<<flush;
-        gettimeofday(&time1, NULL);
+        time1 = chrono::high_resolution_clock::now();
         primsGraph(im1b,ordered,parents,edgemst,step1,m,n,o);
         regularisationCL(costall,u0,v0,w0,u1,v1,w1,hw1,step1,quant1,ordered,parents,edgemst);
-        gettimeofday(&time2, NULL);
-		timeSmooth+=time2.tv_sec+time2.tv_usec/1e6-(time1.tv_sec+time1.tv_usec/1e6);
+        time2 = chrono::high_resolution_clock::now();
+		timeSmooth = time2 - time1;
         cout<<"S"<<flush;
 		
         //FULL-REGISTRATION BACKWARDS
-        gettimeofday(&time1, NULL);
+        time1 = chrono::high_resolution_clock::now();
 		upsampleDeformationsCL(u0,v0,w0,u1i,v1i,w1i,m1,n1,o1,m2,n2,o2);
         upsampleDeformationsCL(ux,vx,wx,u0,v0,w0,m,n,o,m1,n1,o1);
 		warpImageCL(warped1,im1b,warped0,ux,vx,wx);
 		u1i=new float[sz1]; v1i=new float[sz1]; w1i=new float[sz1];
-        gettimeofday(&time2, NULL);
-		timeTrans+=time2.tv_sec+time2.tv_usec/1e6-(time1.tv_sec+time1.tv_usec/1e6);
+        time2 = chrono::high_resolution_clock::now();
+		timeTrans += time2 - time1;
         cout<<"T"<<flush;
-        gettimeofday(&time1, NULL);
+        time1 = chrono::high_resolution_clock::now();
 		descriptor(warped_mind,warped1,m,n,o,mind_step[level]);
 
-        gettimeofday(&time2, NULL);
-		timeMIND+=time2.tv_sec+time2.tv_usec/1e6-(time1.tv_sec+time1.tv_usec/1e6);
+        time2 = chrono::high_resolution_clock::now();
+		timeMIND += time2 - time1;
         cout<<"M"<<flush;
-        gettimeofday(&time1, NULL);
+        time1 = chrono::high_resolution_clock::now();
         dataCostCL((unsigned long*)im1_mind,(unsigned long*)warped_mind,costall,m,n,o,len3,step1,hw1,quant1,args.alpha,RAND_SAMPLES);
-        gettimeofday(&time2, NULL);
-		timeData+=time2.tv_sec+time2.tv_usec/1e6-(time1.tv_sec+time1.tv_usec/1e6);
+        time2 = chrono::high_resolution_clock::now();
+		timeData += time2 - time1;
         cout<<"D"<<flush;
-        gettimeofday(&time1, NULL);
+        time1 = chrono::high_resolution_clock::now();
         primsGraph(warped0,ordered,parents,edgemst,step1,m,n,o);
         regularisationCL(costall,u0,v0,w0,u1i,v1i,w1i,hw1,step1,quant1,ordered,parents,edgemst);
-        gettimeofday(&time2, NULL);
-		timeSmooth+=time2.tv_sec+time2.tv_usec/1e6-(time1.tv_sec+time1.tv_usec/1e6);
+        time2 = chrono::high_resolution_clock::now();
+		timeSmooth += time2 - time1;
         cout<<"S"<<flush;
 		
-        cout<<"\nTime: MIND="<<timeMIND<<", data="<<timeData<<", MST-reg="<<timeSmooth<<", transf.="<<timeTrans<<"\n speed="<<2.0*(float)sz1*(float)len3/(timeData+timeSmooth)<<" dof/s\n";
+        cout << "\nTime: MIND=" << timeMIND.count() << ", data="<< timeData.count() <<", MST-reg="<< timeSmooth.count() <<", transf.="<< timeTrans.count() <<"\n";
+        cout << "speed=" << 2.0*(float)sz1*(float)len3/((timeData+timeSmooth).count()) << " dof/s\n";
         
-        gettimeofday(&time1, NULL);
+        time1 = chrono::high_resolution_clock::now();
         consistentMappingCL(u1,v1,w1,u1i,v1i,w1i,m1,n1,o1,step1);
-        gettimeofday(&time2, NULL);
-        float timeMapping=time2.tv_sec+time2.tv_usec/1e6-(time1.tv_sec+time1.tv_usec/1e6);
+        time2 = chrono::high_resolution_clock::now();
+        chrono::duration<float> timeMapping= time2 - time1;
 		
         //cout<<"Time consistentMapping: "<<timeMapping<<"  \n";
 		
@@ -333,8 +334,8 @@ int main (int argc, char * const argv[]) {
 	//==========================================================================================
 	//==========================================================================================
 	
-    gettimeofday(&time2a, NULL);
-	float timeALL=time2a.tv_sec+time2a.tv_usec/1e6-(time1a.tv_sec+time1a.tv_usec/1e6);
+    time2a = chrono::high_resolution_clock::now();
+	chrono::duration<float> timeALL = time2a - time1a;
     
     upsampleDeformationsCL(ux,vx,wx,u1,v1,w1,m,n,o,m1,n1,o1);
 	
@@ -378,7 +379,7 @@ int main (int argc, char * const argv[]) {
         gzWriteSegment(outputseg,segw,header,m,n,o,1);
     }
     
-	cout<<"Finished. Total time: "<<timeALL<<" sec. ("<<timeDataSmooth<<" sec. for MIND+data+reg+trans)\n";
+	cout<<"Finished. Total time: "<< timeALL.count() <<" sec. ("<< timeDataSmooth.count() <<" sec. for MIND+data+reg+trans)\n";
 	
 	
 	return 0;
